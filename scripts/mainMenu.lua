f2e4f65deea3f509c1a1cfedf6d833b38def37e5
Steffen Majaura <steffen.majaura@gmail.com>
2024-11-06 21:09:53 +0100

    mainmenu colors

--- scripts/mainMenu.lua
+++ scripts/mainMenu.lua
@@ -1,1 +1,32 @@
-header_color = {0.0, 1.0, 0.0, 1.0}
+
+colors = {
+  {0,0,0 ,0},
+  {255,0,0 ,0},
+  {0,255,0 ,0},
+  {0,0,255 ,0},
+  {128,128,128 ,0},
+}
+
+background_color = 2
+
+button_normal_color = 3
+button_hover_color = 4
+button_click_color = 5
+
+font = "dogica"
+
+--        Text,											 Pos,       Size, Color
+texts = {
+  --{"OLC Code Jam 2024",									{0.1, 0.76},  3, 0},
+}
+
+--   File,         Pos,      Scale
+graphics = {
+  --{"pge2_logo", { 0.53, 0.76 }, 0.5}
+}
+
+--  Text,					 Pos,    Size, Color
+buttons = {
+    [0] = {"Box2d ", { 0.1, 0.1 }, 1, 4 }
+  }
+  
