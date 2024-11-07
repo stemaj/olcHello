0313ba42ef5ea5fb5334e7dbd6fc8b3b04db02c5
Steffen Majaura <steffen.majaura@gmail.com>
2024-11-07 10:38:31 +0100

    design main menu and intro

--- scripts/intro.lua
+++ scripts/intro.lua
@@ -1,12 +1,24 @@
-header_color = {0.949, 0.925, 0.545, 1.0}
+font = "dogica"
+fade_time = 1.0
+intro_time = 5.0
 
-olc_logo_pos = { 0.03, 0.8 }
-riegel_pos = { 0.05, 0.8 }
-daddy_pos = { 0.05, 0.8 }
-vfc_pos = { 0.05, 0.8 }
+colors = {
+  {243, 123, 125, 233},
+  {255, 255, 255, 233},
+}
+background_color = 0
 
-name_of_the_game_pos = { 0.05, 0.2 }
-name_of_the_game = "olc Hello"
+--        Name,               Pos,       Size, Start, End, Color
+texts = {
+  {"Introduction ",           { 0.05, 0.8 },  0, 0.3, 0.7, 0},
+}
 
-copyright_pos = { 0.05, 0.85 }
-copyright = "(c) 2024 riegel games"
+--   File,         Pos,      Scale, Start, End
+graphics = {
+  {"pge2_logo", { 0.03, 0.15 }, 0.8, 0.3, 0.7}
+}
+
+-- animations = {
+--   "pretty_pretty_bg_25pc",
+--   --"pretty_pretty_ch_25pc",
+-- }
--- scripts/mainMenu.lua
+++ scripts/mainMenu.lua
@@ -1,32 +1,38 @@
 
 colors = {
-  {0,0,0 ,0},
-  {255,0,0 ,0},
-  {0,255,0 ,0},
-  {0,0,255 ,0},
-  {128,128,128 ,0},
+  --Jehkoba16 Palette
+  {41,128,166 ,255},
+  {240,205,48 ,255},
+  {8,9,26 ,255},
+  {242,236,230 ,255},
+  {152,217,211 ,255},
 }
 
-background_color = 2
+background_color = 0
 
-button_normal_color = 3
-button_hover_color = 4
-button_click_color = 5
+button_normal_color = 2
+button_hover_color = 3
+button_click_color = 4
 
 font = "dogica"
 
 --        Text,											 Pos,       Size, Color
 texts = {
-  --{"OLC Code Jam 2024",									{0.1, 0.76},  3, 0},
+  {"Playground ",									{0.6, 0.12},  3, 1},
 }
 
 --   File,         Pos,      Scale
 graphics = {
-  --{"pge2_logo", { 0.53, 0.76 }, 0.5}
+  {"pge2_logo", { 0.05, 0.05 }, 0.5}
 }
 
---  Text,					 Pos,    Size, Color
+--  Option: Text,					 Pos,    Size, Color
 buttons = {
-    [0] = {"Box2d ", { 0.1, 0.1 }, 1, 4 }
+    [1] = {"Physics ",     { 0.15, 0.25 }, 1, 1 },
+    [2] = {"Pathfinding ", { 0.15, 0.36 }, 1, 1 },
+    [3] = {"Animation ",   { 0.15, 0.47 }, 1, 1 },
+    [4] = {"Objectcursor ",{ 0.15, 0.58 }, 1, 1 },
+    [5] = {"Dialog ",      { 0.15, 0.69 }, 1, 1 },
+    [6] = {"Tap ",         { 0.15, 0.80 }, 1, 1 },
   }
   
