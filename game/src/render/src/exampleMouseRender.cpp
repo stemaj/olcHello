34f04817155f98ab42a7663f4dd054818edbdc81
Steffen Majaura <steffen.majaura@gmail.com>
2024-11-14 15:18:52 +0100

    load mouse

--- game/src/render/src/exampleMouseRender.cpp
+++ game/src/render/src/exampleMouseRender.cpp
@@ -4,34 +4,20 @@
 #include "olcTemplate/game/input.hpp"
 #include "olcTemplate/game/src/render/helper/olcHelper.hpp"
 #include "sol.hpp"
+#include <olcTemplate/game/loadsave.hpp>
 
 using namespace stemaj;
 
 ExampleMouseRender::ExampleMouseRender()
 {
-  sol::state lua;
-  lua.open_libraries(sol::lib::base, sol::lib::io, sol::lib::math, sol::lib::table);
-
-  try
-  {
-    lua.safe_script_file("scripts/exampleMouse.lua");
-  }
-  catch (const sol::error& e)
-  {
-    std::cout << std::string(e.what()) << std::endl;
-  }
-
   _gui->setColNormal(255,255,255,255);
 
-  sol::table buttonsTable = lua["imageButtons"].get_or(sol::table(lua,sol::create));
-  for (auto& [key, values] : buttonsTable)
+  LS.Init("exampleMouse");
+  auto graphBtns = LS.ButtonGraphics();
+  for (const auto& [key,value] : graphBtns)
   {
-    auto p = ((sol::table)values).get_or<std::array<float, 2>>(2,{});
-
-    _gui->prepareImageButton((ButtonAction)key.as<int>(),
-      ((sol::table)values).get_or<std::string>(1,""),
-      CO.D({p[0],p[1]}),
-      ((sol::table)values).get_or(3,0));
+    _gui->prepareImageButton(
+      key, value.file, value.pos, value.buttonDelta);
   }
 }
 
