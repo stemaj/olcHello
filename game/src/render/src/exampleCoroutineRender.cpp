fcfad33b5eda2b07b94401ee0de9ec6147e0ae92
Steffen Majaura <steffen.majaura@bender.de>
2025-02-14 10:10:12 +0100

    img in room

--- game/src/render/src/exampleCoroutineRender.cpp
+++ game/src/render/src/exampleCoroutineRender.cpp
@@ -1,6 +1,8 @@
+#include <olcTemplate/game/src/render/helper/olcHelper.hpp>
 #include <game/src/render/exampleCoroutineRender.hpp>
 #include <olcTemplate/game/src/engine/olcPixelGameEngine.h>
 #include <game/src/state/exampleCoroutineState.hpp>
+#include <olcTemplate/game/assets.hpp>
 
 using namespace stemaj;
 
@@ -34,6 +36,11 @@ float fov = 100.0f;   // Brennweite
     float posX = startX, posY = startY, posZ = startZPos;
     // Geschwindigkeit
     float speed = 800.0f;
+
+    // Decal-Position im 3D-Raum
+    float decalX = -50.0f;
+    float decalY = -10.0f;
+    float decalZ = 200.0f;
 
     // Perspektivische Projektion mit Kameraverschiebung
     olc::vi2d Project(float x, float y, float z) {
@@ -91,6 +98,12 @@ if (isBumping) {
     }
 }
 
+
+    olc::vf2d decalPos = Project(decalX, decalY, decalZ);
+    float scaleFactor = fov / (decalZ - camZ + fov); // Skalierung mit Tiefe
+    pge->DrawDecal(decalPos, AS.Decal("japan_1"), { scaleFactor, scaleFactor });
+
+
   // *** Gitter zeichnen mit steuerbaren Linien ***
   for (float z = startZ; z < depth; z += gridSize) {
     if (drawXLines) { // Vertikale Linien in x-Richtung
@@ -143,10 +156,12 @@ if (distance > 1.0f) { // Stoppen, wenn nahe genug
   float minZ = 20.0f;  // Kleinste Z-Koordinate (nächstgelegene Position)
   float maxZ = 2000.0f; // Größte Z-Koordinate (fernste Position)
   float factor = 1.0f + (1.0f - ((posZ - minZ) / (maxZ - minZ))) * 40.0f;
+  if (factor < 1.0f) factor = 1.0f;
   
   // Kreis zeichnen
   auto objPos = Project(posX, posY, posZ);
   pge->FillCircle(objPos, int(1 * factor), olc::RED);
+  OlcHelper::FillCircleDecal(pge, int(1 * factor), PT<int>{objPos.x, objPos.y}, olc::RED);
 
   // for (auto& ball : s->balls) {
   //   pge->FillCircle(ball.x, ball.y, 10, ball.color); // Ball zeichnen
